f7c2f9e84400c8ca97683889a926faa23967b8f2
ClaudioToledoGHT <69940159+ClaudioToledoGHT@users.noreply.github.com>
2021-07-26 23:16:21 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Teste_2",
+                    Local = "Teste",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
